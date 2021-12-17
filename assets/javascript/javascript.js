7f7ec7bcae03a18439e4c717b982f751ee971217
Juno <junonguyen27@gmail.com>
2021-12-16 19:55:36 -0600

    final touches

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,9 +1,10 @@
 var wantNumbers;
 var wantSpecialChar;
 var wantCaps;
+var wantRegularText;
 var charNum;
 
-character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", " < ", "=", " > ", " ? ", "@", "[","]", " ^ ", "_", "`", "{", "|", "}", "~"];
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[","]", "^", "_", "`", "{", "|", "}", "~"];
 
 number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -11,7 +12,7 @@ alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "
 
 password = [];
 
-var choices;
+choices = [];
 
 var toUpper = function (x) {
     return x.toUpperCase();
@@ -35,10 +36,9 @@ function customPassword() {
   var pickChoices = choices[Math.floor(Math.random() * choices.length)];
   password.push(pickChoices);
   }
-  console.log(charNum);
   
   var passwordText = password.join("");
-  document.querySelector("#password").textContent= password.toString();
+  document.querySelector("#password").textContent= password.join("");
   return passwordText;
 };
 
@@ -46,56 +46,101 @@ function customPassword() {
 function generatePassword() {
   
   charNum = prompt("enter the length of password");
-  if (!(charNum >= 8 && charNum < 128)) {};
+  if ((charNum >= 8 && charNum < 128)) {
+    
+    var specialChar = prompt("do you want special characters?");
+    if (specialChar =="yes") {
+      var wantSpecialChar = true;
+    } else if (specialChar == "no") {
+      var wantSpecialChar = false;
+    };
+    
+    var caps = prompt("do you want uppercase characters?");
+    if (caps=="yes") {
+      var wantCaps = true;
+    } else if (caps=="no") {
+      var wantCaps = false;
+    };
   
-  var specialChar = prompt("do you want special characters?");
-  if (specialChar =="yes") {
-    var wantSpecialChar = true;
-  } else if (specialChar == "no") {
-    var wantSpecialChar = false;
-  };
+    var numbers = prompt("do you want numbers?");
+    if (numbers=="yes") {
+      var wantNumbers = true;
+    } 
+    else if (numbers=="no") {
+      var wantNumbers = false;
+    };
   
-  var caps = prompt("do you want uppercase characters?");
-  if (caps=="yes") {
-    var wantCaps = true;
-  } else if (caps=="no") {
-    var wantCaps = false;
-  };
-
-  var numbers = prompt("do you want numbers?");
-  if (numbers=="yes") {
-    var wantNumbers = true;
-  } else if (numbers=="no") {
-    var wantNumbers = false;
-  };
-  console.log(wantSpecialChar);
-  console.log(wantCaps);
-  console.log(wantNumbers);
-
-  if (wantCaps==true && wantSpecialChar==true && wantNumbers==true) {
-    choices = character.concat(number,alpha,alpha2);
+    var regulartext = prompt("do you want lower case?");
+    if (regulartext=="yes") {
+      var wantRegularText = true;
+    } else if (RegularText = "no") {
+      var wantRegularText = false;
+    };
+  
+    if (wantCaps==true && wantSpecialChar==true && wantNumbers==true && wantRegularText==true) {
+      choices = character.concat(number,alpha,alpha2);
+    } 
+  
+    else if (wantCaps==false && wantSpecialChar==true && wantNumbers==true && wantRegularText==true) {
+      choices = character.concat(number,alpha);
+    } 
+  
+    else if (wantCaps==false && wantSpecialChar==false && wantNumbers==true && wantRegularText==true) {
+      choices = number.concat(alpha);
+    } 
+  
+    else if (wantCaps==false && wantSpecialChar==true && wantNumbers==true && wantRegularText==true) {
+      choices = (alpha);
+    } 
+  
+    else if (wantCaps==true && wantSpecialChar==false && wantNumbers==false && wantRegularText==false) {
+      choices = (alpha2);
+    } 
+  
+    else if (wantCaps==true && wantSpecialChar==true && wantNumbers==false && wantRegularText==false) {
+      choices = character.concat(alpha2);
+    } 
+  
+    else if (wantCaps==true && wantSpecialChar==true && wantNumbers==true && wantRegularText==false) {
+      choices = character.concat(alpha2, number);
+    } 
+  
+    else if (wantCaps==true && wantSpecialChar==false && wantNumbers==false && wantRegularText==true) {
+      choices = alpha2.concat(alpha);
+    } 
+  
+    else if (wantCaps==false && wantSpecialChar==true && wantNumbers==false && wantRegularText==false) {
+      choices = (character);
+    } 
+  
+    else if (wantCaps==false && wantSpecialChar==false && wantNumbers==true && wantRegularText==false) {
+      choices = (numbers);
+    } 
+  
+    else if (wantCaps==false && wantSpecialChar==true && wantNumbers==true && wantRegularText==false) {
+      choices = character.concat(number);
+    } 
+  
+    else if (wantCaps==false && wantSpecialChar==true && wantNumbers==false && wantRegularText==true) {
+      choices = character.concat(alpha);
+    } 
+  
+    else if (wantCaps==true && wantSpecialChar==false && wantNumbers==true && wantRegularText==false) {
+      choices = alpha2.concat(number);
+    } 
+  
+    else if (wantCaps==false && wantSpecialChar==true && wantNumbers==false && wantRegularText==false) {
+      choices = (character);
+    } 
+    
+    else if (wantCaps==false && wantSpecialChar==false && wantNumbers==false && wantRegularText==false) {
+      choices = alert("You need to pick atleast one trait!");
+    } 
   } 
   
-  else if (wantCaps==false && wantSpecialChar==true && wantNumbers==true) {
-    choices = character.concat(number,alpha);
-  } 
-  
-  else if (wantCaps==false && wantSpecialChar==false && wantNumbers==true) {
-    choices = number.concat(alpha);
-  }
-
-  else if (wantCaps==false && wantSpecialChar==false && wantNumbers==false) {
-    choices = (alpha);
-  }
-
-  else if (wantCaps==false && wantSpecialChar==true && wantNumbers==false) {
-    choices = character.concat(alpha);
-  }
-
-  else if (wantCaps==true && wantSpecialChar==true && wantNumbers==false) {
-    choices = character.concat(alpha,alpha2);
-  };
-  
+  else if (!(charNum >= 8 && charNum < 128)) { 
+    choices = alert("You must enter a number between 8 and 128!")};
+   
   var string = customPassword();
 
 
